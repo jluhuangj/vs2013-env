d2fd8e7924527880f86bc5db1a34ffd144457238
jluhuangj <244376116@qq.com>
2018-11-24 19:59:03 +0800

    update

--- main/main.cpp
+++ main/main.cpp
@@ -9,29 +9,29 @@
 //	system("pause");
 //}
 
-//#include <boost/lambda/lambda.hpp>
-//#include <iostream>
-//#include <iterator>
-//#include <algorithm>
-//
-//int main()
-//{
-//	using namespace boost::lambda;
-//	typedef std::istream_iterator<int> in;
-//
-//	std::for_each(
-//		in(std::cin), in(), std::cout << (_1 * 3) << " ");
-//}
-
+#include <boost/lambda/lambda.hpp>
 #include <iostream>
+#include <iterator>
+#include <algorithm>
 
-#include "opencv2/opencv.hpp"
-
-void main()
+int main()
 {
-	cv::Mat img = cv::imread("1.jpg");
-	cv::imshow("1", img);
-	cv::waitKey(0);
+	using namespace boost::lambda;
+	typedef std::istream_iterator<int> in;
 
-	//system("pause");
+	std::for_each(
+		in(std::cin), in(), std::cout << (_1 * 3) << " ");
 }
+
+//#include <iostream>
+//
+//#include "opencv2/opencv.hpp"
+//
+//void main()
+//{
+//	cv::Mat img = cv::imread("1.jpg");
+//	cv::imshow("1", img);
+//	cv::waitKey(0);
+//
+//	//system("pause");
+//}
